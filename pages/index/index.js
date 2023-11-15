f02f4ce6b3eea28fbc0db9988ea82d5b85b0508d
xche529 <126313171+xche529@users.noreply.github.com>
2023-11-16 09:59:06 +1300

    Added testing data

---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+const app = getApp()
+
+Page({
+  data: {
+    transArray: [],
+    transOne:{
+      name: 'S11',
+      capacity: 20, /*单位kVA*/
+      price: 6500,
+      voltage: 10, /* 单位kV */
+      matertial: '全铜'
+    }
+  },
+
+  onClick: function(){
+    this.setData
+  }
+})
