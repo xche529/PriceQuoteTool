b84c9c22c9cac5e26351c22eb17817221ee0e011
xche529 <126313171+xche529@users.noreply.github.com>
2023-11-16 10:41:59 +1300

    Added test logic

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,17 +2,39 @@ const app = getApp()
 
 Page({
   data: {
-    transArray: [],
-    transOne:{
+    transSelectedList: [],
+
+    transList: [
+      {
       name: 'S11',
-      capacity: 20, /*单位kVA*/
+      capacity: 20,
+      /*单位kVA*/
       price: 6500,
-      voltage: 10, /* 单位kV */
-      matertial: '全铜'
-    }
+      voltage: 10,
+      /* 单位kV */
+      matertial: '全铜',
+      id: 1
+    },
+  ]
   },
 
-  onClick: function(){
-    this.setData
+  onAdd: function() {
+    let locelTransList = this.data.transSelectedList;
+    let transOne = this.data.transList[0];
+    locelTransList.push(transOne);
+    console.log('transformer added');
+    console.log(locelTransList);
+    this.setData({
+      transSelectedList: locelTransList
+    })
+  },
+
+  onDelete: function(){
+    let transList = this.data.transSelectedList;
+    transList.pop();
+    console.log('transformer deleted');
+    this.setData({
+      transSelectedList: transList
+    })
   }
 })
