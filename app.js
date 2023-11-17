91a86f98cec7ad89a9196ac410cee44de6b8aadb
xche529 <126313171+xche529@users.noreply.github.com>
2023-11-17 13:06:43 +1300

    Added global data for item list

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    selectedTransformer: null,
+    transSelectedList: [],
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,10 +2,19 @@ const app = getApp()
 
 Page({
   data: {
-    transSelectedList: [],
+    selectedList: [],
+    transList: [{
+      name: 'S11',
+      capacity: 20,
+      /* 单位kVA */
+      price: 6500,
+      voltage: 10,
+      /* 单位kV */
+      material: '全铜',
+      id: 1
+    }],
 
-    transList: [
-      {
+    example: {
       name: 'S11',
       capacity: 20,
       /*单位kVA*/
@@ -15,46 +24,30 @@ Page({
       material: '全铜',
       id: 1
     }
-  ],
-
-  example: {
-    name: 'S11',
-    capacity: 20,
-    /*单位kVA*/
-    price: 6500,
-    voltage: 10,
-    /* 单位kV */
-    material: '全铜',
-    id: 1
-  }
   },
 
-  onAdd: function() {
-    let locelTransList = this.data.transSelectedList;
+  onAdd: function () {
+    let localTransList = getApp().globalData.transSelectedList;
     let transOne = this.data.transList[0];
-    locelTransList.push(transOne);
+    localTransList.push(transOne);
     console.log('transformer added');
-    this.setData({
-      transSelectedList: locelTransList
-    })
+    this.setData({selectedList: localTransList});
   },
 
-  onDelete: function(){
-    let transList = this.data.transSelectedList;
+  onDelete: function () {
+    let transList = getApp().globalData.transSelectedList;
     transList.pop();
     console.log('transformer deleted');
-    this.setData({
-      transSelectedList: transList
-    })
+    this.setData({selectedList: transList});
   },
 
-  onSelect: function(){
+  onSelect: function () {
     wx.navigateTo({
       url: '/pages/select/select'
     });
   },
 
-  onResult: function(){
+  onResult: function () {
     wx.navigateTo({
       url: '/pages/result/result'
     });
