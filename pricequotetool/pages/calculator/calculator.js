0f7051f84444bd11a626caabfacc9021d3a68662
xche529 <126313171+xche529@users.noreply.github.com>
2023-11-24 12:35:11 +1300

    Added info update system for the calculator

--- pricequotetool/pages/calculator/calculator.js
+++ pricequotetool/pages/calculator/calculator.js
@@ -7,17 +7,22 @@ Page({
       numberArray.push(i.toString());
     }
     this.setData({
-      costFactorArray: numberArray
+      pickerArray: numberArray
     });
   },
 
   data: {
-    totalPrice : 0,
-    quantity : 0,
-    tax: 15,
-    costFactorArray: [],
+    totalPrice: 0.00,
+    finalPrice: 0.00,
+    finalPriceTaxed: 0.00,
+    priceDifference: 0.00,
+    quantity: 0,
+    taxCost: 0.00,
+    taxRate: 15,
+    pickerArray: [],
     selectedList: [],
     inputCostFactor: '',
+    inputTax: '',
     transList: [{
       price: 6500,
       costFactor: 100,
@@ -37,6 +42,7 @@ Page({
     this.setData({
       selectedList: localTransList
     });
+    this.updateAll();
   },
 
   onDeleteTransformer: function (event) {
@@ -46,6 +52,7 @@ Page({
     this.setData({
       selectedList: selectedList
     });
+    this.updateAll();
   },
 
   onChangePriceFactor: function (event) {
@@ -58,6 +65,33 @@ Page({
       selectedList: selectedList
     });
     console.log('new factor:', costFactor)
+    this.updateAll();
+  },
+
+  onChangePrice: function (event) {
+    let costFactor = event.detail.value - 100;
+    console.log('new factor:', costFactor)
+    let index = event.currentTarget.dataset.index;
+    let selectedList = this.data.selectedList;
+    selectedList[index].costFactor = costFactor;
+    this.setData({
+      selectedList: selectedList
+    });
+    console.log('new factor:', costFactor)
+    this.updateAll();
+  },
+
+  onChangeQuantity: function (event) {
+    let costFactor = event.detail.value - 100;
+    console.log('new factor:', costFactor)
+    let index = event.currentTarget.dataset.index;
+    let selectedList = this.data.selectedList;
+    selectedList[index].costFactor = costFactor;
+    this.setData({
+      selectedList: selectedList
+    });
+    console.log('new factor:', costFactor)
+    this.updateAll();
   },
 
   onInputAllCostFactor: function (event) {
@@ -65,18 +99,37 @@ Page({
     this.setData({
       inputCostFactor: value
     })
-    console.log(value)
-    console.log(this.data.inputCostFactor)
   },
 
+  onInputTax: function (event) {
+    let value = event.detail.value;
+    this.setData({
+      inputTax: value
+    })
+  },
+
+  onUpdateTax: function () {
+    let numericValue = parseFloat(this.data.inputTax);
+    console.log(numericValue)
+    if (!isNaN(numericValue)) {
+      this.setData({
+        taxRate: numericValue
+      })
+    } else {
+      wx.showToast({
+        title: '请输入正确的数字',
+        icon: 'none',
+        duration: 2000,
+      });
+    }
+    this.emptyTaxInput();
+    this.updateAll();
+  },
 
   onUpdateAllCostFactor: function (event) {
     let numericValue = parseFloat(this.data.inputCostFactor);
     console.log(numericValue)
     if (!isNaN(numericValue)) {
-      this.setData({
-        inputCostFactor: numericValue
-      })
       this.updateAllCostFactor(numericValue);
     } else {
       wx.showToast({
@@ -86,6 +139,7 @@ Page({
       });
       this.emptyCostFactorInput();
     }
+    this.updateAll();
   },
 
   updateAllCostFactor: function (value) {
@@ -104,5 +158,52 @@ Page({
     this.setData({
       inputCostFactor: empty
     });
+  },
+  emptyTaxInput: function () {
+    let empty = '';
+    this.setData({
+      inputTax: empty
+    });
+  },
+  updateAll: function(){
+    this.updateTotalPrice();
+    this.updateFinalCost();
+    this.updateQuantity();
+    this.priceDifference = this.finalPrice - this.totalPrice;
+    this.finalPriceTaxed = parseFloat(this.finalPrice) + (parseFloat(this.finalPrice) * parseFloat(this.data.taxRate) / 100);
+    this.taxCost = this.finalPriceTaxed - this.finalPrice;
+    this.setData({
+      totalPrice: this.totalPrice,
+      finalPrice: this.finalPrice,
+      priceDifference: this.priceDifference,
+      finalPriceTaxed: this.finalPriceTaxed,
+      taxCost: this.taxCost,
+      quantity: this.quantity
+    })
+  },
+
+  updateTotalPrice: function(){
+    let totalCost = 0
+    this.data.selectedList.forEach(function (transformer) {
+      totalCost += transformer.price;
+    }, this);
+    totalCost = totalCost.toFixed(2);
+    this.totalPrice= totalCost;
+  },
+  updateFinalCost: function(){
+    let finalPrice = 0
+    this.data.selectedList.forEach(function (transformer) {
+      finalPrice += transformer.price * transformer.costFactor / 100;
+    }, this);
+    finalPrice = finalPrice.toFixed(2);
+    this.finalPrice = finalPrice;
+  },
+  
+  updateQuantity: function(){
+    let quantity = 0
+    this.data.selectedList.forEach(function (transformer) {
+      quantity += transformer.quantity;
+    }, this);
+    this.quantity = quantity;
   }
 })
