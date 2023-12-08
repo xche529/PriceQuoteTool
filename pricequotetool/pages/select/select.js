d85ea9360c12a627636a9b72566874f582997b3f
xche529 <126313171+xche529@users.noreply.github.com>
2023-12-08 15:03:56 +1300

    Created search function

--- pricequotetool/pages/select/select.js
+++ pricequotetool/pages/select/select.js
@@ -1,8 +1,14 @@
 const db = wx.cloud.database()
+const _ = db.command;
 Page({
   data: {
-    tempList:[],
+    selectedCapacityIndex: '',
+    selectedCapacityValue: '',
+
+    tempList: [],
     searchResult: null,
+    capacityArray: null,
+
     template: {
       name: null,
       capacity: null,
@@ -13,11 +19,25 @@ Page({
       material: null,
       id: null
     }
-
   },
 
   onLoad: function () {
+    db.collection('searchInfo').get({
+      success: (res) => {
+        this.data.capacityArray = res.data[0].capacity;
+        console.log(res.data[0].capacity)
+        this.data.selectedCapacityIndex = Math.floor(res.data[0].capacity.length / 2);
+        console.log(this.data.selectedCapacityIndex)
 
+        this.data.selectedCapacityValue = res.data[0].capacity[this.data.selectedCapacityIndex];
+        console.log(this.data.selectedCapacityValue);
+        this.setData({
+          capacityArray: this.data.capacityArray,
+          selectedCapacityIndex: this.data.selectedCapacityIndex,
+          selectedCapacityValue: this.data.selectedCapacityValue,
+        })
+      }
+    })
   },
 
   onSearch: function () {
@@ -26,16 +46,26 @@ Page({
         console.log(res.data)
         this.searchResult = res.data;
         this.setResult();
+      },
+      fail: (error) => {
+        console.error(error);
       }
-      
     })
   },
 
-  setResult: function(){
+  onSelectCapacity: function (event) {
+    this.data.selectedCapacityIndex = event.detail.value;
+    this.data.selectedCapacityValue = this.data.capacityArray[this.data.selectedCapacityIndex];
+    this.setData({selectedCapacityValue:this.data.selectedCapacityValue})
+  },
+
+  setResult: function () {
     this.data.tempList = [];
     this.searchResult.forEach((transformer) => {
-      let temp = {...this.data.template};
-      temp.name = transformer.type;x
+      let temp = {
+        ...this.data.template
+      };
+      temp.name = transformer.type;
       temp.price = transformer.price;
       temp.capacity = transformer.capacity;
       temp.material = transformer.material;
@@ -43,7 +73,7 @@ Page({
       this.data.tempList.push(temp);
     });
     this.setData({
-      tempList : this.data.tempList
+      tempList: this.data.tempList
     });
     console.log(this.data.tempList);
   }
