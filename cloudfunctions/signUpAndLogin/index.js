a11999c1c6e4dcd6fdcc7b4e86e9517a8c6f3dfd
xche529 <126313171+xche529@users.noreply.github.com>
2023-12-19 11:56:13 +1300

    Added login state for login page

--- cloudfunctions/signUpAndLogin/index.js
+++ cloudfunctions/signUpAndLogin/index.js
@@ -8,24 +8,61 @@ const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
   let signedUp = null;
+  let name = '数据加载失败';
+  let avatar = '';
   const wxContext = cloud.getWXContext();
   const isSignUp = event.isSignUp;
+  //判断是否为注册行为
   if (isSignUp) {
-
+    try {
+      userInfo = {
+        openID: wxContext.OPENID,
+        avatar: event.avatar,
+        name: event.name,
+      }
+      const res = await db.collection('users').where({
+        openID: wxContext.OPENID
+      }).get();
+      if (!res.data.length > 0) {
+        await db.collection('users').add({
+          data: userInfo
+        })
+        signedUp = true;
+      } else {
+        signedUp = true;
+      }
+    } catch {
+      signedUp = false;
+    }
   } else {
     try {
       const res = await db.collection('users').where({
         openID: wxContext.OPENID
       }).get();
-
       signedUp = res.data.length > 0;
     } catch (error) {
       console.error('Error querying database:', error);
       signedUp = false;
     }
   }
+
+  if (signedUp) {
+    try {
+      const user = await db.collection('users').where({
+        openID: wxContext.OPENID
+      }).get();
+
+      name = user.data[0].name
+      avatar = user.data[0].avatar
+    } catch {
+      signedUp = false
+    }
+  }
+
   return {
     msg: 'ok',
-    signedUp
+    signedUp,
+    name,
+    avatar,
   }
 }
--- pricequotetool/pages/login/login.js
+++ pricequotetool/pages/login/login.js
@@ -2,31 +2,68 @@
 Page({
 
   data: {
+    // 控制页面状态
     isLogin: false,
     isSignUp: false,
     isAdmin: false,
+    name: '加载失败',
+    avatar: '',
+
     tempNickName: '',
     tempAvatar: '../../image/Search.jpg',
   },
 
+
   onLoad() {
+    //检测用户是否注册
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    })
+
     wx.cloud.callFunction({
       name: 'signUpAndLogin',
       data: {
         isSignUp: false
       },
-      success: res =>{
+      success: res => {
         console.log(res)
+        if (res.result.signedUp) {
+          this.setData({
+            name: res.result.name,
+            avatar: res.result.avatar,
+            isLogin: true
+          })
+        }
+        wx.hideLoading()
+      },
+      fail: res => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '加载失败！',
+          icon: error
+        })
       }
     })
   },
 
+  // 用户注册使用
   onSignUp() {
-    this.setData({
-      isSignUp: true
+    wx.cloud.callFunction({
+      name: 'signUpAndLogin',
+      data: {
+        isSignUp: true,
+        avatar: this.data.tempAvatar,
+        name: this.data.tempNickName,
+      },
+      success: res => {
+        console.log(res)
+        console.log(this.data.tempNickName)
+      }
     })
   },
 
+  // 存储用户昵称
   onInputNickName(event) {
     let value = event.detail.value;
     this.setData({
@@ -34,21 +71,19 @@ Page({
     })
   },
 
+  // 存储用户选择头像
   onLoadAvatar(event) {
     const avatar = event.detail.avatarUrl
     console.log(avatar)
-    this.setData ({
+    this.setData({
       tempAvatar: avatar
     })
   },
 
-  onLogin(){
-    wx.cloud.callFunction({
-      name: 'signUpAndLogin',
-      data: {
-        avatar: this.data.tempAvatar,
-        name: this.data.tempNickName
-      }
+  // 用于用户登录
+  onLogin() {
+    this.setData({
+      isSignUp: true
     })
   }
 })
