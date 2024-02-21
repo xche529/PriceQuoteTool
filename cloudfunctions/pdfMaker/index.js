37d2826f74eb3107dfd3a8c8cfdc239d5e4cce52
xche529 <126313171+xche529@users.noreply.github.com>
2024-02-21 22:32:17 +1300

    Changed pdfMaker function

--- cloudfunctions/pdfMaker/index.js
+++ cloudfunctions/pdfMaker/index.js
@@ -1,11 +1,17 @@
 // 云函数入口文件
 
 const fs = require('fs');
-const {
-  PDFDocument,
-} = require('pdf-lib');
-const fontkit = require('@pdf-lib/fontkit');
+var pdfMake = require('pdfmake')
 const cloud = require('wx-server-sdk')
+var fonts = {
+  Roboto: {
+    normal: 'fonts/Roboto-Regular.ttf',
+    bold: 'fonts/Roboto-Medium.ttf',
+    italics: 'fonts/Roboto-Italic.ttf',
+    bolditalics: 'fonts/Roboto-MediumItalic.ttf'
+  }
+};
+var printer = new pdfMake(fonts);
 cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV
 }) // 使用当前云环境
@@ -13,54 +19,38 @@ const db = cloud.database();
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  let number = 0;
   const transformers = event.products
   transformers.forEach(transformer => {
     number++;
   });
   const wxContext = cloud.getWXContext();
 
-  const fontBytes = fs.readFileSync('./font.ttf');
-  const pdfDoc = await PDFDocument.create();
-  pdfDoc.registerFontkit(fontkit);
-  const font = await pdfDoc.embedFont(fontBytes);
-  const page = pdfDoc.addPage();
-  let {
-    width,
-    height
-  } = page.getSize();
-  const fontSize = 30
-  page.drawText('共选择 ' + number + ' tranformers', {
-    x: 50,
-    y: height - 4 * fontSize,
-    size: fontSize,
-    font: font
-  })  
-  transformers.forEach(transformer => {
-    height = height - fontSize
-    page.drawText('规格型号： ' + transformer.name + ' 材料： ' + transformer.material + ' 容量： ' + transformer.capacity + ' 价格： ' + transformer.price, {
-      x: 50,
-      y: height - 4 * fontSize,
-      size: fontSize - 20,
-      font: font
-    })
+  const docDefinition = {
+    content: [
+      'Test',
+      {
+        table: {
+          body: [
+            ['c1', 'c2', 'c3'],
+            ['c11', 'c12', 'c13'],
+            ['c21', 'c22', 'c23']
+          ]
+        }
+      }
+    ]
+  }
 
+  var pdfDoc = printer.createPdfKitDocument(docDefinition)
+  const tempFilePath = `/tmp/document.pdf`;
+
+  // 将 PDF 写入到临时文件中
+  const writeFile = promisify(fs.writeFile);
+  await writeFile(tempFilePath, pdfDoc);
+
+  const result = await storage.uploadFile({
+    cloudPath: 'document.pdf',
+    filePath: tempFilePath, 
   });
 
-  const pdfBytes = await pdfDoc.save()
-  const base64Data = Buffer.from(pdfBytes).toString('base64');
-  const random = Math.random().toString(36).substr(2, 15);
-  const fileName = random + '.json'
-  const res = await cloud.uploadFile({
-    cloudPath: fileName,
-    fileContent: JSON.stringify({
-      data: base64Data
-    }),
-
-  })
-  const id = res.fileID;
-  return {
-    id,
-    fileName,
-  }
-}
+  return result.fileID;
+};
